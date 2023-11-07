786a5af3f99e2fe387d96945a0db911498466de8
theosAnyone <theo.fernandez.perso@gmail.com>
2023-11-07 12:02:12 +0100

    url changed

--- src/app/api/apiSlice.js
+++ src/app/api/apiSlice.js
@@ -2,7 +2,7 @@ import {createApi, fetchBaseQuery} from '@reduxjs/toolkit/query/react'
 import { setCredentials } from '../../features/auth/authSlice'
 
 const baseQuery = fetchBaseQuery({
-    baseUrl: 'https://anyone-dashboard.onrender.com',
+    baseUrl: 'https://dashboard-api-m5pk.onrender.com',
     credentials: 'include',
     prepareHeaders: (headers, { getState }) => {
         const token = getState().auth.token
