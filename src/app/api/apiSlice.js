b8b00439aa0466be6a8ab5a70f31b3218afd57d0
theosAnyone <theo.fernandez.perso@gmail.com>
2023-11-10 18:06:09 +0100

    url

--- src/app/api/apiSlice.js
+++ src/app/api/apiSlice.js
@@ -2,17 +2,17 @@ import {createApi, fetchBaseQuery} from '@reduxjs/toolkit/query/react'
 import { setCredentials } from '../../features/auth/authSlice'
 
 const baseQuery = fetchBaseQuery({
-    baseUrl: 'http://localhost:3500',
-    credentials: 'include',
-    prepareHeaders: (headers, { getState }) => {
-        const token = getState().auth.token
-        console.log("token:",token);
-        if (token) {
-            headers.set("authorization", `Bearer ${token}`)
-        }
-        return headers
+  baseUrl: "https://dashboard-api-m5pk.onrender.com",
+  credentials: "include",
+  prepareHeaders: (headers, { getState }) => {
+    const token = getState().auth.token;
+    console.log("token:", token);
+    if (token) {
+      headers.set("authorization", `Bearer ${token}`);
     }
-})
+    return headers;
+  },
+});
 
 const baseQueryWithReauth = async (args, api, extraOptions) => {
     // console.log(args) // request url, method, body
