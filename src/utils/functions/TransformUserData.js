fdf8c54793716173c7c0de76116dfc3000d02583
theosAnyone <theo.fernandez.perso@gmail.com>
2023-11-18 13:49:35 +0100

    gestion du status des eleves qui sont au dernier exercice du parcours

--- src/utils/functions/TransformUserData.js
+++ src/utils/functions/TransformUserData.js
@@ -1,7 +1,13 @@
+const global_var = require('../global_var.json')
+
+function bloc_is_reviewed(bloc){
+  return bloc?.reviews && bloc.reviews.length > 0;
+}
+
 export default function transform_users_data(users){
-  
+  const JOURNEY_PROGRESS_CAP = global_var.JOURNEY_PROGRESS_CAP
   const user_transformed_data = users.map(user => {
-        
+    const user_blocs_length = user.Journey_Infos.blocs?.length
     const name = user.Discord_Infos.displayName
     const img = user.Discord_Infos.avatar_url || require('../../img/discord.svg')
 
@@ -22,7 +28,7 @@ export default function transform_users_data(users){
       }
     }
 
-    const status = user.Journey_Infos.blocs?.length ?  (user.Journey_Infos.blocs.some(bloc=>(!bloc.reviewed && !bloc.reviews?.length)&& bloc.completed) ? "Not reviewed" : user.Journey_Infos.blocs.some(bloc=>!bloc.reviewed) ? 'In progress' : user.Journey_Infos.progress === 19 ? 'Finished' : 'Reviewed') : 'Just started';
+    const status = !user_blocs_length ? 'Just started' :  (user.Journey_Infos.blocs.some(bloc=> !bloc_is_reviewed(bloc) && bloc.completed) || (user.Journey_Infos.progress === JOURNEY_PROGRESS_CAP && bloc_is_reviewed(user.Journey_Infos.blocs[user_blocs_length - 1]) ))  ? "Not reviewed" : user.Journey_Infos.blocs.some(bloc=>!bloc.reviewed) ? 'In progress' : (user.Journey_Infos.progress === JOURNEY_PROGRESS_CAP && !user.Journey_Infos.blocs.some(bloc => !bloc_is_reviewed(bloc)))  ? 'Finished' : 'Reviewed' ;
     const fiche_state_color = (one_good && one_wrong) ? '🟠' : !one_wrong ? '🟢' : '🔴' ;
     const progress = user.Journey_Infos.progress
     const subscription = user.Discord_Infos.grade
@@ -33,3 +39,4 @@ export default function transform_users_data(users){
   })
   return user_transformed_data
 }
+
